3577ccdd1aac2ca38c6a985e9226fb2a33feeb98
jakefroeb <jake.herman.froeb@gmail.com>
2021-02-25 15:39:12 -0600

    finished chapter 8 adding customer to the AnimalCard module

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -21,9 +21,13 @@ export const ApplicationViews = () => {
 
             {/* Render the animal list when http://localhost:3000/animals */}
             <AnimalProvider>
-                <Route exact path="/animals">
-                    <AnimalList />
-                </Route>
+                <LocationProvider>
+                    <CustomerProvider>
+                        <Route exact path="/animals">
+                            <AnimalList />
+                        </Route>
+                    </CustomerProvider>
+                </LocationProvider>
             </AnimalProvider>
             <CustomerProvider>
                 <Route path="/customers">
